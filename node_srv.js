05f792cb97258f19c120c8e3de627a05e086310b
luytk12 <koenluyten@hotmail.com>
2015-07-07 14:38:22 +0200

    test git

---
+++ node_srv.js
@@ -0,0 +1,21 @@
+// helper function
+function padNumber(num) {
+      return ("0000" + num).slice(-3);
+}
+
+var url = require('url');
+var net = require('net');
+var esp = net.connect(9000, '192.168.1.15',
+      function() {
+            console.log('connected');
+      });
+
+var http = require('http');
+http.createServer(function(req, res) {
+      var q = url.parse(req.url, true).query;
+      esp.write('D:' + padNumber(q.r) + padNumber(q.g) + padNumber(q.b));
+      res.writeHead(200, {
+            'Content-Type': 'text/plain'
+      });
+      res.end('OK');
+}).listen(8888, '0.0.0.0');
