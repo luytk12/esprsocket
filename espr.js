56dedb71937ddb4d5db7351a570e113a58000cca
Koen <koenluyten@hotmail.com>
2015-08-21 10:22:22 +0200

    test

--- espr.js
+++ espr.js
@@ -1,17 +1,38 @@
 //dev
 //espruino
-server = require("net").createServer(function(soc­ket) {
+
+//  client = require("net").connect({host : "192.168.1.50", port: 1883},
+var server = require("net").createServer(function(soc­ket) {
   socket.on('data', function(data) {
-       parseData(data);
+    parseData(data);
   });
+
+  var ow = new OneWire(A1);
+  var sensor = require("DS18B20").connect(ow);
+  var readTempInterval = setInterval(function() {
+    console.log("Publishing " + sensor.getTemp());
+    socket.write(sensor.getTemp());
+  }, 5000);
+
+  socket.on('end', function()
+  {
+    clearInterval(readTempInterval);
+    console.log('client disconnected');
+  });
+
 }).listen(9000);
 
+
+
 function parseData(data) {
+  //parse
+  //read-write
+  //command execute
   var idx = data.indexOf('D:', data.length - 11);
   tRGB[0] = data.substr(idx + 2, 3);
   tRGB[1] = data.substr(idx + 5, 3);
   tRGB[2] = data.substr(idx + 8, 3);
 
   console.log('r: ' + tRGB[0] + ' g: ' + tRGB[1] + ' b: ' + tRGB[2]);
-  initColor(); // This fades the color a bit, and sends the pattern through SPI to the LED controller.
+  // initColor(); //send to controller
 }
--- node_srv.js
+++ node_srv.js
@@ -1,15 +1,20 @@
 //dev
 //node
-// Helper function to format the string that we'll send to the Espruino
+
 function padNumber(num) {
+    // helper function
     return ("0000" + num).slice(-3);
 }
+
 var url = require('url');
 var net = require('net');
 var esp = net.connect(9000, '192.168.1.15',
         function() {
-          console.log('Connected to Espruino!');
+          console.log('connected!');
 });
+
+//listen pattern: ?r=#&g=#&b=# on port 8888
+//send pattern: D:RRRGGGBBB on port 9000
 var http = require('http');
 http.createServer(function (req, res) {
       var q = url.parse(req.url, true).query;
